e1e9a3b4fca3d10c07c001c1c91dc5aacd74d0a8
noakrams <noakrams96@gmail.com>
2020-09-30 11:53:26 +0300

    Saving temporary changes - have different sizes for large-hero
    Signed-off-by: noakrams <noakrams96@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/images/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        before: function(app, server){
+            server._watch('./app/**/*.html')
+        },
+        host: '0.0.0.0'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
